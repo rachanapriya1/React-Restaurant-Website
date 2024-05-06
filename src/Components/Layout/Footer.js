43c02f7e8372cf26e48bf349e27753fbfcedd1e5
Rachana Martha <martharachanapriya@gmail.com>
2024-05-06 17:18:46 +0530

    first commit

---
+++ src/Components/Layout/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Box, Typography }  from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+
+const Footer = () => {
+  return (
+   <>
+   <Box sx={{textAlign:'center',bgcolor:'#1A1A19',color:'white',p:3}}>
+    <Box sx={{my :3, "& svg":{
+        fontSize : "60px",
+        cursor : 'pointer',
+        mr : 2,
+    },
+    "& svg : hover":{
+       color : 'orange',
+       transform : 'translateX(5px)',
+       transition : "all 500ms",
+    }
+    }}>
+    {/* icons */}
+    <InstagramIcon />
+    <TwitterIcon />
+    <YouTubeIcon />
+    <GitHubIcon />
+
+    </Box>
+    
+    <Typography variant='h5' sx={{"@media (max-width:600px)":{fontSize:'1rem'}}}>
+        All rights reserved &copy; Techinfo YT 
+        </Typography>
+   </Box>
+   </>
+  )
+}
+
+export default Footer
---
+++ src/data/data.js
@@ -0,0 +1,99 @@
+import Dosa from "../Images/dosa.jpg"
+import Chola from "../Images/chhola.jpg";
+import Idli from "../Images/idli.jpg";
+import MasalaDosa from "../Images/masala.jpg";
+import Paneer from "../Images/paneer.jpg";
+import Gujrati from "../Images/gujrati.jpeg";
+import Chicken from "../Images/chicken.jpeg";
+import Soup from"../Images/soup.jpeg";
+import Sweets from "../Images/sweets.jpeg";
+import Cake from "../Images/Cake.jpeg";
+import Icecream from "../Images/Icecream.jpeg";
+import FullMeals from "../Images/meals.jpeg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Chola",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chola,
+    price: 250,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Gujrati,
+    price: 500,
+  },
+  {
+    name: "Chicken Biryani",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chicken,
+    price: 250,
+  },
+  {
+    name: "Cake",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Cake,
+    price: 300,
+  },
+  {
+    name: "Ice Cream",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Icecream,
+    price: 200,
+  },
+  {
+    name: "Sweets",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Sweets,
+    price: 250,
+  },
+  {
+    name: "Corn Soup",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Soup,
+    price: 250,
+  },
+  {
+    name: "Meals",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: FullMeals,
+    price: 300,
+  },
+];
